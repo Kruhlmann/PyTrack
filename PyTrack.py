a3a7a4515c8bcfd7f9520f41dbeda7d9143fbbb6
Andreas Krühlmann <andreaskruhlmann@gmail.com>
2017-08-23 00:41:44 +0200

    Init

---
+++ PyTrack.py
@@ -0,0 +1,47 @@
+import gspread
+import requests
+import socket
+import datetime
+import getpass
+from oauth2client.service_account import ServiceAccountCredentials
+
+def get_last_row(sheet):
+	for i in range(2, sheet.row_count):
+		if sheet.cell(i, 1).value == "":
+			return i
+	return -1
+
+def get_ip_address():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	ip_address = s.getsockname()[0]
+	s.close()
+	return ip_address
+
+def track(ip_address, sheet, row):
+	request_json = requests.get('http://ip-api.com/json/' + ip_address).json()
+	sheet.update_cell(row, 1, str(row_index - 1))
+	sheet.update_cell(row, 2, str(datetime.datetime.now()))
+	sheet.update_cell(row, 3, ip_address)
+	sheet.update_cell(row, 4, request_json['isp'])
+	sheet.update_cell(row, 5, getpass.getuser())
+	sheet.update_cell(row, 6, socket.gethostname())
+	sheet.update_cell(row, 7, request_json['country'])
+	sheet.update_cell(row, 8, request_json['regionName'])
+	sheet.update_cell(row, 9, request_json['city'])
+	sheet.update_cell(row, 10, request_json['zip'])
+	sheet.update_cell(row, 11, request_json['timezone'])
+	sheet.update_cell(row, 12, str(request_json['lat']) + "/" + str(request_json['lon']))
+
+if __name__ == "__main__":
+	scope = ["https://spreadsheets.google.com/feeds"]
+	credentials = ServiceAccountCredentials.from_json_keyfile_name("client_credentials.json", scope)
+	client = gspread.authorize(credentials)
+
+	sheet = client.open('PyTrack').sheet1
+	row_index = get_last_row(sheet)
+	if row_index == -1:
+		print("Error: Couldn't find last row in sheet.")
+		exit()
+	ip_address = get_ip_address()
+	track(ip_address, sheet, row_index)
